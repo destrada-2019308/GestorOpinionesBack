94464f3fcd4b5b3a3bc607082116ed535b9e4a69
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2024-06-17 13:14:31 -0600

    modificando index.js

--- index.js
+++ index.js
@@ -4,20 +4,3 @@ import { connect } from './configs/mong.js'
 initServer()
 connect()
 
-import User from './src/user/user.model.js'
-import { encrypt } from './src/utils/validator.js'
-
-const registerDef = async() => {
-    const encrypty = await encrypt('adminadmin')
-    const newUser = new User({name: "asd",
-        lastname: "asd",
-        username: "admin",
-        password: encrypty,
-        email: "asd",
-        phone: "12345678",
-        role: "CLIENT"
-    })
-    await newUser.save()
-}
-
-registerDef()
